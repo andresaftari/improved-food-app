e4a5af5a49b31ecd25007051432b518e79be4da6
andresaftari <andresaftari@gmail.com>
2020-10-03 23:39:10 +0700

    Replace Lint with getString() for profile

--- app/src/main/java/com/andresaftari/foodiezz/view/about/AboutFragment.kt
+++ app/src/main/java/com/andresaftari/foodiezz/view/about/AboutFragment.kt
@@ -1,13 +1,12 @@
 package com.andresaftari.foodiezz.view.about
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.andresaftari.foodiezz.R
 import kotlinx.android.synthetic.main.fragment_about.*
 
@@ -17,21 +16,20 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_about, container, false)
 
-    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Set the textview with my contact
-        tv_nameAbout.text = "PRASIDYA PRAMADRESANA SAFTARI"
-        tv_linkedIn.text = "https://www.linkedin.com/in/prasidya-pramadresana-saftari/"
-        tv_emailAbout.text = "andresaftari@gmail.com"
-        tv_contactAbout.text = "instagram.com/andresaftari"
+        tv_nameAbout.text = getString(R.string.profile_name)
+        tv_linkedIn.text = getString(R.string.profile_link)
+        tv_emailAbout.text = getString(R.string.profile_email)
+        tv_contactAbout.text = getString(R.string.profile_instagram)
 
         // Redirect the user to my LinkedIn page
         tv_linkedIn.setOnClickListener {
             startActivity(
                 Intent(Intent.ACTION_VIEW).setData(
-                    Uri.parse("https://www.linkedin.com/in/prasidya-pramadresana-saftari/")
+                    Uri.parse(getString(R.string.profile_link))
                 )
             )
         }
@@ -40,7 +38,7 @@ class AboutFragment : Fragment() {
         tv_contactAbout.setOnClickListener {
             startActivity(
                 Intent(Intent.ACTION_VIEW).setData(
-                    Uri.parse("https://www.instagram.com/andresaftari/")
+                    Uri.parse(getString(R.string.profile_instagram))
                 )
             )
         }
